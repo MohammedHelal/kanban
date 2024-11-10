d966344ca230f6f523873581605a8ecc8b5a5e29
MohammedHelal <mohd.omar.helal@gmail.com>
2024-11-10 14:21:11 +0800

    fixing path

--- src/app/[board]/page.jsx
+++ src/app/[board]/page.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import LoadingSkeleton from "@/src/ui/LoadingSkeleton";
 
 import { useContext } from "react";
-import { BoardTaskContext } from "../store/board-task-context";
+import { BoardTaskContext } from "@/src/store/board-task-context";
 
 export default function Page() {
   const { loading } = useContext(BoardTaskContext);
